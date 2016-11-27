14fd522b0388ba300130921c4ef1cec88247bafc
Jeferson Cunha <me@jefersoncunha.com>
2016-11-27 19:22:44 -0200

    home ajusts

--- home.php
+++ home.php
@@ -10,9 +10,9 @@
 			<div class="container">
       <?php include "inc/views/_partials/menu.php" ?>
         <div class="jumbotron">
-					<h2>Controle de Pagamentos</h2>
-          <p>O Sistema é uma protopico inicial 1.0, para um controle de pagamentos de meus clientes.</p>
-          <p> Nesse sistema é possível fazer controle de : Categorias, Clientes e Pagamentos </p>
+					<h2>Controle de Clientes</h2>
+          <p>O Sistema é uma protopico inicial 1.0, para um controle  clientes.</p>
+          <p> Nesse sistema é possível fazer controle completo de Clientes, bem como efetuar buscas. </p>
 
 				</div>
       <?php include "inc/views/_partials/rodape.php" ?>
